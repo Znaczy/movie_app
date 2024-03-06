3283949b3fb64c18a14bc45db21698daa5692ed6
Krzysztof <honotu@gmail.com>
2024-03-06 17:13:43 +0100

    zmiana projektu

--- lib/domain/entities/movie_entity.dart
+++
@@ -1,8 +0,0 @@
-class Movie{
-  final int id;
-  final String title;
-  final String overview;
-  final String posterPath;
-
-  Movie({required this.id, required this.title, required this.overview, required this.posterPath, });
-}
--- lib/domain/repositories/movie_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:movie_app/domain/entities/movie_entity.dart';
-
-abstract class MovieRepository{
-  Future<List<Movie>> getTrendingMovies();
-  Future<List<Movie>> searchMovie(String query);
-  Future<List<Movie>> getPopularMovies();
-}
